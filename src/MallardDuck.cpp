0de3b843a0a020aa2a22f227417a4d8064d103bc
cfair <cfair@hach.com>
2017-01-13 14:36:10 -0700

    added empty files for implementations

--- src/MallardDuck.cpp
+++
@@ -1,9 +0,0 @@
-#include "MallardDuck.h"
-
-bool MallardDuck::Display()
-{
-    std::cout<<"I am a happy mallard duck!"<<std::endl;
-    return true;
-}
-
-
--- src/RedHeadDuck.cpp
+++
@@ -1,9 +0,0 @@
-#include "RedHeadDuck.h"
-
-bool RedHeadDuck::Display()
-{
-    std::cout<<"I am a depressed redhead duck!"<<std::endl;
-    return true;
-}
-
-
