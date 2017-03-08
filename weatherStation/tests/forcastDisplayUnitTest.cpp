1631b0ae44f14bb8e051bc643182bde608909c9b
Christopher Fair <cfair@hach.com>
2017-03-08 09:09:12 -0700

    added the modules for code coverage

--- weatherStation/tests/forcastDisplayUnitTest.cpp
+++ weatherStation/tests/forcastDisplayUnitTest.cpp
@@ -10,42 +10,43 @@
 #undef protected
 #undef private 
 
-#include "catch.hpp"
+#include "catch.hpp" // This is required for all unit test code
 
 
 TEST_CASE ( "Forcast Display Unit Tests", "[forcastDisplay]" ) {
-  WeatherStation::WeatherData * weatherData = new WeatherStation::WeatherData();
-  WeatherStation::ForcastDisplay * forcastDisplay = new WeatherStation::ForcastDisplay(weatherData);
+   WeatherStation::WeatherData * weatherData = new WeatherStation::WeatherData();
+   WeatherStation::ForcastDisplay * forcastDisplay = new WeatherStation::ForcastDisplay(weatherData);
 
-  weatherData->setMeasurements(10, 10, 10.0f);
-  weatherData->setMeasurements(20, 20, 20.0f);
-  weatherData->setMeasurements(30, 30, 30.0f);
-  weatherData->setMeasurements(40, 40, 40.0f);
-  weatherData->setMeasurements(50, 50, 50.0f);
+   weatherData->setMeasurements(10, 10, 10.0f);
+   weatherData->setMeasurements(20, 20, 20.0f);
+   weatherData->setMeasurements(30, 30, 30.0f);
+   weatherData->setMeasurements(40, 40, 40.0f);
+   weatherData->setMeasurements(50, 50, 50.0f);
 
-  const int averageTemperature = forcastDisplay->getAverage(forcastDisplay->historicalTemperatures);
-  const int averageHumidities = forcastDisplay->getAverage(forcastDisplay->historicalHumidities);
-  const int averagePressures = forcastDisplay->getAverage(forcastDisplay->historicalPressures);
+   const int averageTemperature = forcastDisplay->getAverage(forcastDisplay->historicalTemperatures);
+   const int averageHumidities = forcastDisplay->getAverage(forcastDisplay->historicalHumidities);
+   const int averagePressures = forcastDisplay->getAverage(forcastDisplay->historicalPressures);
 
-  const bool thatAllAveragesAreThirty = ((averageTemperature == 30) &&
-                                         (averageHumidities == 30) &&
-                                         (averagePressures == 30));
+   const bool thatAllAveragesAreThirty = ((averageTemperature == 30) &&
+                                          (averageHumidities == 30) &&
+                                          (averagePressures == 30));
 
-  const bool numberOfTemperatures = forcastDisplay->historicalTemperatures.size() == 5;
-  const bool numberOfHumidities = forcastDisplay->historicalHumidities.size() == 5;
-  const bool numberOfPressures = forcastDisplay->historicalPressures.size() == 5;
+   const bool numberOfTemperatures = forcastDisplay->historicalTemperatures.size() == 5;
+   const bool numberOfHumidities = forcastDisplay->historicalHumidities.size() == 5;
+   const bool numberOfPressures = forcastDisplay->historicalPressures.size() == 5;
 
 
-  const bool thatThereAreFiveReadings = numberOfTemperatures &&
-    numberOfHumidities &&
-    numberOfPressures;
+   const bool thatThereAreFiveReadings = numberOfTemperatures &&
+                                         numberOfHumidities &&
+                                         numberOfPressures;
 
-  REQUIRE( thatAllAveragesAreThirty ); // average should be thirty for all members
-  REQUIRE( thatThereAreFiveReadings );  
+   // These are the actual unit tests the rest is really set up
+   REQUIRE( thatAllAveragesAreThirty ); // average should be thirty for all members
+   REQUIRE( thatThereAreFiveReadings );  
 
-  (void)forcastDisplay;
-  delete forcastDisplay;
-  delete weatherData;
+   (void)forcastDisplay;
+   delete forcastDisplay;
+   delete weatherData;
 }
 
 
