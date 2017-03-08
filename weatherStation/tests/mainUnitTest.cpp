017b5bb3fbc529946e449578672f28b95636aa87
cfair <christopherpfair@comcast.net>
2017-03-07 20:27:44 -0700

    renamed the unit test files

---
+++ weatherStation/tests/mainUnitTest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "WeatherData.h"
+#include "CurrentConditionsDisplay.h"
+#include "StatisticsDisplay.h"
+#include "ForcastDisplay.h"
+#include "HeatIndexDisplay.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+
