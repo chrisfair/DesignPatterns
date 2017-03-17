738df2762b6e07110e97abd9e25097ae2d3e92f6
Christopher Fair <cfair@hach.com>
2017-03-17 15:33:05 -0600

    tried again to limit the coverage list

--- weatherStation/atmel-build/runUnitTest.sh
+++ weatherStation/atmel-build/runUnitTest.sh
@@ -47,7 +47,7 @@ lcov -r $DIRECTORY_FOR_LCOV/coverage.info.filtered "4.8.6" -o $DIRECTORY_FOR_LCO
 
 geninfo $DIRECTORY_FOR_LCOV/ -o $DIRECTORY_FOR_LCOV/lcov.dat
 cd $DIRECTORY_FOR_LCOV
-lcov_cobertura -b $BASENAME_FOR_COBERTURA $DIRECTORY_FOR_LCOV/lcov.dat -e catch,catch-test,arm-linu
+lcov_cobertura -b $BASENAME_FOR_COBERTURA $DIRECTORY_FOR_LCOV/lcov.dat -e "*catch*","*catch-test*", "*4.8.2*" 
 cp coverage.xml $1
 genhtml $DIRECTORY_FOR_LCOV/coverage.info.filtered -o $DIRECTORY_FOR_LCOV/lcovHtml
 
