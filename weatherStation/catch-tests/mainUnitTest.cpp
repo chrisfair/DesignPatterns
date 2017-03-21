1205244d584ac882919bd42cef9f9e2702fc0374
Christopher Fair <cfair@hach.com>
2017-03-21 09:58:11 -0600

    removed some unnecessary headers in main unittest

--- weatherStation/catch-tests/mainUnitTest.cpp
+++ weatherStation/catch-tests/mainUnitTest.cpp
@@ -3,13 +3,6 @@
 // in one file, so you only need to do something special here
 // if you need a custom main function beyond the created one in
 // catch.
-#include<iostream>
-#include "WeatherData.h"
-#include "CurrentConditionsDisplay.h"
-#include "StatisticsDisplay.h"
-#include "ForcastDisplay.h"
-#include "HeatIndexDisplay.h"
-
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
